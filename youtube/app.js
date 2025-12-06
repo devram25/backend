cb81ecd5f0ac78c60289bf7b9efcbab1262c5922
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-12-06 11:30:55 +0530

    health check route

--- youtube/app.js
+++ youtube/app.js
@@ -10,4 +10,13 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 
+// Inport Routes
+import healthcheckRoute from "./routes/healthcheckRoutes.js";
+
+
+
+
+// routes
+app.use("/api/v1/healthcheck", healthcheckRoute)
+
 export { app };
