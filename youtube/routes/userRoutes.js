eac68d39f84424ee7f745c7957da780724e3bc3c
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-12-26 21:45:58 +0530

    add getCurrentUser route and update user routes for better organization

--- youtube/routes/userRoutes.js
+++ youtube/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logoutUser, registerUser } from "../controllers/userController.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/userController.js";
 import upload from "../middleware/multer.js";
 import { verifyJWT } from "../middleware/authmiddleware.js";
 
@@ -16,7 +16,18 @@ userRoute.route("/register").post(
   registerUser
 );
 
+userRoute.post("/login", loginUser)
+userRoute.post("refresh-token", refreshAccessToken)
+
+
 // secured routes
 userRoute.route("/logout").post(verifyJWT, logoutUser)
+userRoute.post("/change-password", verifyJWT, changeCurrentPassword)
+userRoute.get("/current-user",verifyJWT, getCurrentUser )
+userRoute.get("/c/:username", verifyJWT, getUserChannelProfile)
+userRoute.patch("/update-account", verifyJWT, updateAccountDetails)
+userRoute.patch("/avatar", verifyJWT,upload.single("avatar"), updateUserAvatar)
+userRoute.patch("/cover-image", verifyJWT,upload.single("coverImage") , updateUserCoverImage)
+userRoute.get("/history", verifyJWT, getWatchHistory)
 
 export default userRoute;
