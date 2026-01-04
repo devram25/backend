4885316c52e1fa44be7e138af087556af68e3cf1
Ramprasad yadav <yadavramprasad563@gmail.com>
2026-01-04 10:57:52 +0530

    api rate limit

--- youtube/app.js
+++ youtube/app.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 const app = express();
 import cookieParser from "cookie-parser";
-
+import {rateLimit} from 'express-rate-limit'
 
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials:true}));
 
+const limiter =  rateLimit({
+    windowMs: 10*60*1000,
+    limit:10
+})
 // common middlewares
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(limiter)
 
 // Inport Routes
 import healthcheckRoute from "./routes/healthcheckRoutes.js";
