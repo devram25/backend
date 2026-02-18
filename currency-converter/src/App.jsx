985efba80f7f42bbc41a92bc2f4409f4fc8b70b4
Ramprasad yadav <yadavramprasad563@gmail.com>
2026-02-18 23:54:01 +0530

    next-auth-app

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState } from "react";
 import { useCurrencyInfo } from "./hooks/useCurrencyInfo";
+import InputBox from "./components/InputBox";
 
 const App = () => {
 	const [ammount, setAmmount] = useState(0)
@@ -8,14 +10,24 @@ const App = () => {
 	const [convertedAmmount, setConvertedAmmount] = useState(0)
 
 	const currencyInfro = useCurrencyInfo(from)
+	// const options = Object.keys(currencyInfro)
+	// console.log(options)
+ console.log(currencyInfro)
+    const Swap =()=>{
+		setFrom(to)
+		setTo(from)
+		setConvertedAmmount(ammount)
+		setAmmount(convertedAmmount)
+	}
+	
+	const Convert =()=>{
+		setConvertedAmmount(ammount * currencyInfro[to])
+	}
 	return (
-		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
-			<img
-				className="size-40"
-				src="https://res.cloudinary.com/dltj8bim0/image/upload/v1761060580/logo_kukwt0.png"
-				alt=""
-			/>
-			<p>Hello Vite + React + TailwindCSS!</p>
+		<div className="w-full" >
+          <form onSubmit={(e)=>{e.preventDefault()}}>
+			<InputBox />
+		  </form>
 		</div>
 	);
 };
---
+++ next-auth-app/src/dbConfig/dbConfig.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+export async function connect(){
+    try {
+        mongoose.connect(process.env.mongo_url!)
+        const connection = mongoose.connection
+         connection.on('connected',()=>{
+            console.log("Connection successfull")
+         })
+    } catch (error) {
+        console.log(error)
+    }
+}
